a4c4dd3808e97e478bb98b8ccb7588e69d327a5c
haibinhpham <10421006@student.vgu.edu.vn>
2024-06-09 00:06:53 +0200

    tenda-test-demo package created with 2 tester guis

---
+++ lib/demo/screens/note_list.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class NoteList extends StatefulWidget {
+  const NoteList({super.key});
+
+  @override
+  State<NoteList> createState() => _NoteListState();
+}
+
+class _NoteListState extends State<NoteList> {
+  int count = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('notes'),
+        backgroundColor: Colors.purple,
+      ),
+      body: getNoteListView(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => {debugPrint('Test button')},
+        tooltip: 'Add Note',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+
+  ListView getNoteListView() {
+    TextStyle? titleStyle = Theme.of(context).textTheme.titleSmall;
+    return ListView.builder(
+      itemCount: count,
+      itemBuilder: (BuildContext context, int position) {
+        return Card(
+          color: Colors.white,
+          elevation: 2.0,
+          child: ListTile(
+            leading: const CircleAvatar(
+              backgroundColor: Colors.yellow,
+              child: Icon(Icons.keyboard_arrow_right),
+            ),
+            title: Text('Dummy', style: titleStyle),
+            subtitle: const Text('Dummy date'),
+            trailing: const Icon(
+              Icons.delete,
+              color: Colors.grey,
+            ),
+            onTap: () => {debugPrint('Hello this is test')},
+          ),
+        );
+      },
+    );
+  }
+}
